1396b0a78a386f2fb07fb3ee698c8681d35e337a
João Fernandes <joao.fernandes450@gmail.com>
2021-06-12 03:59:25 +0100

    Locale improvements

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, Inject, OnInit, ViewChild, ViewContainerRef } fr
 import { HomepageComponent } from '../homepage/homepage.component';
 
 import { LOCALE_ID } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface Language {
   locale: string,
@@ -21,13 +22,19 @@ export class NavigationBarComponent implements OnInit {
   private locale: string = '';
 
   languagesAvailable: Language[] = [
-    { locale: 'en', name: "English", flag: "flag-icon flag-icon-gb", selected: false, href: "/en" },
-    { locale: 'pt', name: "Portuguese", flag: "flag-icon flag-icon-pt", selected: false, href: "/pt" }
+    { locale: 'en', name: "English", flag: "flag-icon flag-icon-gb", selected: false, href: "https://joaofernandes450.github.io/my-portfolio/en/" },
+    { locale: 'pt', name: "Portuguese", flag: "flag-icon flag-icon-pt", selected: false, href: "https://joaofernandes450.github.io/my-portfolio/pt/" }
   ]
 
-  constructor(@Inject(LOCALE_ID) locale: string) {
+  constructor(@Inject(LOCALE_ID) locale: string, private router: Router) {
     this.locale = locale;
-    this.languagesAvailable.find(x => x.locale === this.locale)!.selected = true;
+    const temp = location.href.split('/');
+    if (temp[temp.length - 2] == this.locale) {
+      this.languagesAvailable.find(x => x.locale === this.locale)!.selected = true
+    } else {
+      this.languagesAvailable.find(x => x.locale != this.locale)!.selected = true;
+      localStorage.setItem('Language', locale);
+    }
   }
 
   ngOnInit(): void {
